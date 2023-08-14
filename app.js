8a77fb4a65ac6402bf4ddef95870ea0678b3a171
Vinicius Alves do Amaral <AVA397@einstein.br>
2023-08-14 13:25:27 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogo")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um console log de teste
 console.log("Mais um commit")
+console.log("Enviando para o GitHub")
 
